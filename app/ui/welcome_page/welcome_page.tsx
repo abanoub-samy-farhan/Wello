8ff12aca677a0604d6ab99de211cfc749f38efee
AbanoubAziz <abanoubsamy2341@gmail.com>
2024-12-20 10:11:07 +0200

    Making HomePage Welcome Statmenment and action buttons

--- app/ui/welcome_page/welcome_page.tsx
+++ app/ui/welcome_page/welcome_page.tsx
@@ -1,10 +1,23 @@
+"use client"
 import "../general.css";
+import { GetStartedButton, LearnMoreButton } from "../buttons";
 
 export default function WelcomePage(){
     return (
-        <div className="bg-white h-full w-full p2nd m-0 p-2 p4th-bg">
-            <h1>Welcome to the Welcome Page!</h1>
-            <p>This is the Welcome Page, where you can find all the information you need to get started with the app.</p>
+        <div className="">
+            <div className="bg-white h-full w-full p2nd m-0 p-2 p4th-bg flex flex-col gap-4 p-20">
+            <div className="flex flex-col gap-2 mb-3 max-w-2xl">
+                <h1 className="text-8xl font-extrabold p1st">Welcome to <br /> Wello</h1>
+                <p className="px-1">
+                    Simplify your finances with Wello—a secure, user-friendly platform that unifies your transactions, 
+                    payment management, and financial history in one place. <br />
+                    Revolutionize your banking experience today! </p>
+            </div>
+                <div className="flex flex-row gap-2 justify-center w-fit h-100">
+                    <GetStartedButton />
+                    <LearnMoreButton />
+                </div>
+            </div>
         </div>
     );
 }
